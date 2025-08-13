7729357238d282586ca37a1db842e0e859521bf3
EmreKaplan67 <emrekaplan96@gmail.com>
2025-08-13 16:42:31 +0100

    Prepared for the deployment

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,23 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import connectDB from "./config/db.js";
-import { PORT, FRONTEND_URL } from "./config/env.js";
+import { PORT, FRONTEND_URL, NODE_ENV } from "./config/env.js";
 import errorMiddleware from "./middleware/errorMiddleware.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path";
 
 const app = express();
+const __dirname = path.resolve();
 
-app.use(cors({
-    origin: FRONTEND_URL,
-    credentials: true
-}));
+if (NODE_ENV === "development") {
+    // We need this for development mode cause in development mode we are running our frontend and backend on different ports
+    app.use(cors({
+        origin: FRONTEND_URL,
+        credentials: true
+    }));
+}
 
 app.use(cookieParser());
 app.use(express.json());
@@ -25,6 +30,14 @@ app.use("/api/auth", authRoutes);
 
 app.use(errorMiddleware);
 
+// For our Backend to serve our Frontend (Only for production)
+if (NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
+
 
 app.listen(PORT, () => {
     try {
@@ -33,5 +46,5 @@ app.listen(PORT, () => {
         console.error("Error connecting to the database: ", error);
         process.exit(1);
     }
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT} in ${NODE_ENV} mode`);
 });
--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+
 const api = axios.create({
-    baseURL: "http://localhost:5000/api", // backend url
+    baseURL: BASE_URL,
     withCredentials: true, // this send cookies with requests
 });
 
