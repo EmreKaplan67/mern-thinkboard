3cab9836fb707edfcb2522756b5888c7f63d60b4
EmreKaplan67 <emrekaplan96@gmail.com>
2025-11-27 01:28:28 +0000

    disable rate limiting temporarily

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(cors({
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(rateLimiter);
+// app.use(rateLimiter); // Temporarily disabled
 
 //Routes
 app.use("/api/notes", notesRoutes);
